31f54f2fa5e2f247b0116b46bede2c6bc34705ef
Isaac Martin <isaac.martin@fanduel.com>
2019-11-26 18:07:51 -0500

    move global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import App from 'next/app';
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
-import GlobalStyle from '../components/globalStyle';
+import GlobalStyle from '../theme/globalStyle';
 import theme from '../theme';
 
 export default class MyApp extends App {
